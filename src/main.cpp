a202b9f044512c3ac82dbddd841f41c32a7463c7
merttakipsan <100185120+merttakipsan@users.noreply.github.com>
2022-03-23 17:06:09 +0300

    özel haberleşme komutları girildi

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,24 @@
 #include <Arduino.h>
 
+
 #include "string.h"
 
 char message[20];
-int distance;   //actual distance measurements of LiDAR
-int strength;   //signal strength of LiDAR
-int temperature;    //temperature of LiDAR
+
 int CheckSum;  
 
 
 int received_data[9];    //datas measured by LiDAR
 const int HEADER=0x59;  //frame header of data package
 
- #define RX1 16
-#define TX1 17
+ #define RX2 17
+#define TX2 16
 
  //#define RX1 33
  //#define TX1 25 //lidar rx
 
- #define RX2 21
- #define TX2 22
+ #define RX1 21
+ #define TX1 22
 
 
 int dist;                     //actual distance measurements of LiDAR
@@ -30,7 +29,7 @@ int uart[9];                   //save data measured by LiDAR
 
 
 int dist1;                     //actual distance measurements of LiDAR
-int strength1;                 //signal strength of LiDAR
+                 //signal strength of LiDAR
 int check1;                    //save check value
 int i;
 int uart1[9];                   //save data measured by LiDAR
@@ -41,10 +40,13 @@ void setup() {
   Serial.begin(115200);
   Serial1.begin(115200,SERIAL_8N1,RX1,TX1);
   Serial2.begin(115200,SERIAL_8N1,RX2,TX2);
+  pinMode(12,OUTPUT);
+  pinMode(13,OUTPUT);
   pinMode(4,OUTPUT);
  
 }
-void Read_Lidar(){
+
+void lidars(){
 
   if(Serial1.available()||Serial2.available()){
      
@@ -98,17 +100,28 @@ else{
   dist1=0;
 }
           
-       
+ 
   
 }
+else{
+       dist=0;
+       dist1=0;  
+ }
+/*
+Serial.print("[04:00:01;");
+Serial.print(dist);              
+Serial.print(":02;");
+Serial.print(dist1);     
+Serial.print(']');
+*/
 
-Serial.print("dist 1  = ");
-          Serial.print(dist);              
-         
-          Serial.print(" ****  dist 2  = ");
-          Serial.print(dist1);     
-          Serial.print('\n');
- delay(10);
+String first="[04:00:01;";
+String third=":02;";
+String _final="]";
+
+Serial.println(first+dist+third+dist1+_final);
+
+delay(10);
 
 }
 
@@ -131,19 +144,29 @@ String Q;
  }
 
 Q = readString;
-if (Q=="1"){
+if (Q=="[04:01:01:01]"){
+  digitalWrite(12,HIGH);
+ }
+
+if (Q=="[04:01:01:00]"){
+ digitalWrite(12,LOW);
+}
+if (Q=="[04:01:02:01]"){
+  digitalWrite(13,HIGH);
+ }
+
+if (Q=="[04:01:02:00]"){
+ digitalWrite(13,LOW);
+}
+
+if (Q=="[04:01:03:01]"){
   digitalWrite(4,HIGH);
  }
 
-if (Q=="2"){
+if (Q=="[04:01:03:00]"){
  digitalWrite(4,LOW);
 }
-
-if (Q=="3"){
-  
- }
-
-       Read_Lidar();
+       lidars();
      
     
       
